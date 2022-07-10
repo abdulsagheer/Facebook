91a25f4cbe819307a37c4455f91a4dce8956dea0
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-10 20:47:55 +0530

    Added Email Validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,21 @@
 const User = require('../models/User')
+const {validateEmail} = require('../helpers/validation')
 
 exports.register = async (req, res) => {
     try {
         const {first_name, last_name, email, password, username, bYear, bMonth, bDay, gender} = req.body;
+        if(!validateEmail(email)) {
+            return res.status(400).json({
+                message: 'Invalid email address'
+            })
+        }
+        const check = await User.findOne({ email })
+        if(check) {
+            return res.status(400).json({
+                message: "This email address is already in use.",
+            });
+        }
+        return;
         const user = await new User({
             first_name,
             last_name,
