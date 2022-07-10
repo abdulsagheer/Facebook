fd49bef8cba42a9940ed2aca5c10a818f27d48ae
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-10 20:59:40 +0530

    Implemented validations on email,first & lastname

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,20 +1,60 @@
+// Importing Models
 const User = require('../models/User')
-const {validateEmail} = require('../helpers/validation')
+// Importing Helper functions
+const {validateEmail, validateLength} = require('../helpers/validation')
+// Importing Libraries
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
     try {
-        const {first_name, last_name, email, password, username, bYear, bMonth, bDay, gender} = req.body;
-        if(!validateEmail(email)) {
+        const {
+            first_name,
+            last_name,
+            email,
+            password,
+            username,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+        } = req.body;
+
+        // Implementing email validation
+        if (!validateEmail(email)) {
             return res.status(400).json({
-                message: 'Invalid email address'
-            })
-        }
-        const check = await User.findOne({ email })
-        if(check) {
-            return res.status(400).json({
-                message: "This email address is already in use.",
+            message: "Invalid email address",
             });
         }
+        const check = await User.findOne({ email });
+
+        // Check if the email address already exists
+        if (check) {
+            return res.status(400).json({
+            message: "This email address is already in use.",
+            });
+        }
+
+        // Validating the length first name and last name
+        if (!validateLength(first_name, 3, 30)) {
+            return res.status(400).json({
+            message: "The first name must be at least 3 characters long.",
+            });
+        }
+        if (!validateLength(last_name, 3, 30)) {
+            return res.status(400).json({
+            message: "The last name must be at least 3 characters long.",
+            });
+        }
+
+        // Validating the length of password
+        if (!validateLength(password, 6, 40)) {
+            return res.status(400).json({
+            message: "The password must be at least 6 characters long.",
+            });
+        }
+
+        const cryptedPassword = await bcrypt.hash(password, 12);
+        console.log(cryptedPassword);
         return;
         const user = await new User({
             first_name,
