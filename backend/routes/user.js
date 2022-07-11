609f5ace2759498dbf46fc977522c5b0d37dc358
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-11 19:34:24 +0530

    Configured route for login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/register", activateAccount);
-router.post("/register", login);
+router.post("/login", login);
 
 module.exports = router;
