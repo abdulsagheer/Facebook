dcced70e841dbee0f419b7b9ba60c91b445f22cf
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-11 19:38:18 +0530

    Adde routes to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/home';
+import Login from '../pages/login';
+import Profile from '../pages/profile';
+
 function App() {
 
-  const get = async() => {
-    const res = await fetch("http://localhost:8000/");
-    console.log(res)
-  }
-  get();
-
   return (
-    <div className="App">
-      Welcome to frontend
-      <div className="trash_icon"></div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+      </Routes>
     </div>
   );
 }
