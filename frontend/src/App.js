4b59f30e111a117a7eaf138850fcfb73ec7ac24a
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-11 20:55:31 +0530

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/home';
-import Login from '../pages/login';
-import Profile from '../pages/profile';
+import Home from './pages/home';
+import Login from './pages/login';
+import Profile from './pages/profile';
 
 function App() {
 
