eb12f266a9a4124f1c28efaf2db1f4eeb0634273
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-12 21:44:47 +0530

    initiated redux store and provided initial value

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,21 @@ import "./index.css"
 import "./styles/icons/icons.css"
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import { Provider } from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers';
 
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
