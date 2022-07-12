f66b96643e1858d40d4a442553804205a8771f8b
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-12 21:45:04 +0530

    Added login component

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,9 +1,47 @@
+// Importing style
 import "./style.css";
+// Importing Libraries
+import { Formik, Form } from "formik";
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../../icons/facebook.svg" alt="Facebook" />
+            <span>
+              Facebook helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit" className="blue_btn">Login</button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot" className="forgot_password">
+                Forgotten password
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a Page </b>
+              for a celebrity, brand or business
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="signup_wrapper"></div>
+    </div>
+  );
 }
 
 export default Login
