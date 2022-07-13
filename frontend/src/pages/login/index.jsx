8928d5acbe3004f17bd9dc7aa7f661399854d83f
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-13 13:52:05 +0530

    Added login functionality using Login component

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -3,8 +3,32 @@ import "./style.css";
 // Importing Libraries
 import { Formik, Form } from "formik";
 import {Link} from 'react-router-dom'
+import { useState } from "react";
+import * as Yup from 'yup';
+// Importing components
+import LoginInput from "../../components/inputs/logininput";
+
+const loginInfos = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
+  
+  const [login, setLogin] = useState(loginInfos)
+  const {email, password} = login;
+  const handleLoginChange = e => {
+    const {name, value} = e.target.value;
+    setLogin({...login, [name]: value})
+  }
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required("Email address is required")
+      .email("Must be valid email")
+      .max(100),
+    password: Yup.string()
+      .required("Password is required")
+  });
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -17,12 +41,25 @@ const Login = () => {
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
-              <Formik>
+              <Formik initialValues={{email, password}} validationSchema={loginValidation}>
                 {(formik) => (
                   <Form>
-                    <input type="text" />
-                    <input type="text" />
-                    <button type="submit" className="blue_btn">Login</button>
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Email address or Phone number"
+                      onChange={handleLoginChange}
+                    />
+                    <LoginInput
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleLoginChange}
+                      bottom
+                    />
+                    <button type="submit" className="blue_btn">
+                      Login
+                    </button>
                   </Form>
                 )}
               </Formik>
