8b9d2470ea0f3da9315a96e03889bf0b3a24b9df
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-13 15:44:06 +0530

    Added Desktop and Mobile

--- frontend/src/components/inputs/logininput/index.jsx
+++ frontend/src/components/inputs/logininput/index.jsx
@@ -1,20 +1,34 @@
-import { ErrorMessage, useField } from 'formik';
-import './style.css';
+// Importing styles
+import "./style.css";
+// Importing Libraries
+import { useField, ErrorMessage } from "formik";
+import { useMediaQuery } from "react-responsive";
 
-const LoginInput = ({ placeholder, bottom, ...props}) => {
-    const [field, meta] = useField(...props);
+export default function LoginInput({ placeholder, bottom, ...props }) {
+
+  const [field, meta] = useField(props);
+  const desktopView = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
 
   return (
     <div className="input_wrap">
       {meta.touched && meta.error && !bottom && (
-        <div className="input_error" style={{ transform: "translateY(2px)" }}>
+        <div
+          className={
+            desktopView ? "input_error input_error_desktop" : "input_error"
+          }
+          style={{ transform: "translateY(3px)" }}
+        >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className="error_arrow_top"></div>
+            <div
+              className={desktopView ? "error_arrow_left" : "error_arrow_top"}
+            ></div>
           )}
         </div>
       )}
-
+      
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
         type={field.type}
@@ -24,18 +38,29 @@ const LoginInput = ({ placeholder, bottom, ...props}) => {
         {...props}
       />
       {meta.touched && meta.error && bottom && (
-        <div className="input_error" style={{ transform: "translateY(2px)" }}>
+        <div
+          className={
+            desktopView ? "input_error input_error_desktop" : "input_error"
+          }
+          style={{ transform: "translateY(2px)" }}
+        >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className="error_arrow_bottom"></div>
+            <div
+              className={
+                desktopView ? "error_arrow_left" : "error_arrow_bottom"
+              }
+            ></div>
           )}
         </div>
       )}
+
       {meta.touched && meta.error && (
-        <i className="error_icon" style={{ top: `${!bottom && "63%"}` }}></i>
+        <i
+          className="error_icon"
+          style={{ top: `${!bottom && !desktopView ? "63%" : "15px"}` }}
+        ></i>
       )}
     </div>
   );
 }
-
-export default LoginInput
