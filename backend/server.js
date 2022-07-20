5d352a63db49aa56daf2513e9f34d020923bcfb1
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-20 22:17:03 +0530

    Used file upload library to upload a file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -8,18 +9,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 //routes
-readdirSync("./routes").map((r) => app.use("/user", require("./routes/" + r)));
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
 //database
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-}).then(() => console.log("database connected successfully"))
-.catch((err) => console.log("error connecting to mongodb", err));
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
 
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}..`);
+  console.log(`server is running on port ${PORT}..`);
 });
